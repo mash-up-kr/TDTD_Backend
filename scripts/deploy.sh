a58c9ee2fa77291dbfd4e5161ac70aa13b5caf04
연정 <yjeongisme@gmail.com>
2021-02-24 05:08:33 +0900

    chore: Application 실행 명령어 권한 부여
    - "java.net.SocketException: Permission denied" 발생해서 "sudo" 명령어를 통해 권한 부여함

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \
+sudo nohup java -jar \
         -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-prod.yml \
         -Dspring.profiles.active=prod \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
