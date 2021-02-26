57492be71533118a774c0ed5d99717fe8652b890
YJeongKim <yjeongisme@gmail.com>
2021-02-27 01:23:38 +0900

    fix: 프로세스 ID 조회 오류
    - 배포 과정에서 Application의 PID를 조회할 때 `grep jar`로 검색하면 찾지 못하는 오류가 발생.'jar' > 'java'로 조회하여 해결
    
    - sudo로 프로세스 kill 권한 부여

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/TDTD_Deploy/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
@@ -17,7 +17,7 @@ if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
+    sudo kill -15 $CURRENT_PID
     sleep 5
 fi
 
