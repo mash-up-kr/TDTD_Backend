cab835c7f74825a19c19dc5c91c80bafccd8bf87
jong yoon <dhorlawhddbs@naver.com>
2021-03-03 19:38:21 +0900

    feat: 자기자신의 댓글 삭제 예외처리 리팩토링
    - NoSuchElementException 모호함으로 인한 IllegalArgumentException 변경
    - 커스텀 예외를 만드는 것보다 간단한 메시지 처리가 더 이득일 것으로
    판단하여 기존 IllegalArgumentException를 사용
    - 모든 예외를 BadRequest로 보낼때 모호함이 생기므로 예외처리에 넣어둔
    메시지를 바디에 넣어 보내기 위해 ExceptionResponse 생성

---
+++ src/main/kotlin/mashup/backend/tdtd/common/dto/ExceptionResponse.kt
@@ -0,0 +1,9 @@
+package mashup.backend.tdtd.common.dto
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
+class ExceptionResponse(
+    val errMsg: String
+)
