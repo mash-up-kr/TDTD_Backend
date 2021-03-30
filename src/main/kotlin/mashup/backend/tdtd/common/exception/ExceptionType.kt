8e09613c6798b6781f433b2ca8bdcc5cd0b8ca60
YJeongKim <yjeongisme@gmail.com>
2021-03-30 21:22:03 +0900

    refactor: Report 관련 API에 응답 Formatting 적용
    - Report API의 응답 형태 변경 (테스트 완료)
    - User가 이미 신고한 Comment는 다시 신고할 수 없도록 처리
    - 이미 블라인드된 Comment는 카운트, 업데이트 로직 패스 처리
    - SeedData에 변경된 로직 반영 및 코드 style formatting

--- src/main/kotlin/mashup/backend/tdtd/common/exception/ExceptionType.kt
+++ src/main/kotlin/mashup/backend/tdtd/common/exception/ExceptionType.kt
@@ -7,13 +7,15 @@ enum class ExceptionType(
     BAD_REQUEST(4000, "Bad Request"),
     DEVICE_BAD_REQUEST(4001, "Device-Id is not included."),
     WRONG_PATH_BAD_REQUEST(4002, "The request was sent with the wrong path."),
-    DELETE_COMMENT_BAD_REQUEST(4003, "Can't delete other people's comments."),
+    DELETE_COMMENT_BAD_REQUEST(4003, "Can't delete other people's comment."),
+    REPORT_COMMENT_BAD_REQUEST(4004, "This comment has already been reported."),
 
     UNAUTHORIZED(4010, "Unauthorized"),
     USER_UNAUTHORIZED(4011, ""),
 
     FORBIDDEN(4030, "Forbidden"),
     NON_HOST_USER_FORBIDDEN(4031, "Only hosts can be requested."),
+    REPORT_COMMENT_FORBIDDEN(4032, "Cannot report own comment."),
 
     NOT_FOUND(4040, "Not Found"),
     USER_NOT_FOUND(4041, "The user does not exist."),
--- src/main/kotlin/mashup/backend/tdtd/report/service/ReportService.kt
+++ src/main/kotlin/mashup/backend/tdtd/report/service/ReportService.kt
@@ -2,6 +2,9 @@ package mashup.backend.tdtd.report.service
 
 import mashup.backend.tdtd.comment.entity.Comment
 import mashup.backend.tdtd.comment.service.CommentService
+import mashup.backend.tdtd.common.exception.BadRequestException
+import mashup.backend.tdtd.common.exception.ExceptionType
+import mashup.backend.tdtd.common.exception.ForbiddenException
 import mashup.backend.tdtd.report.entity.Report
 import mashup.backend.tdtd.report.repository.ReportRepository
 import mashup.backend.tdtd.user.service.UserService
@@ -17,16 +20,23 @@ class ReportService(
     companion object {
         const val BLINDED_CONDITION = 3;
         const val BLINDED_STATE = true;
-        const val DELETE_OWN_COMMENT_EXCEPTION_MSG = "Cannot delete own comments."
     }
 
     @Transactional
     fun reportCommentByCommentId(commentId: Long, deviceId: String) {
         val userId: Long = userService.getUserByDeviceId(deviceId).id!!
         val reportedComment: Comment = commentService.getCommentById(commentId)
-        if(userId == reportedComment.userId) throw IllegalArgumentException(DELETE_OWN_COMMENT_EXCEPTION_MSG)
+        if (userId == reportedComment.userId) throw ForbiddenException(ExceptionType.REPORT_COMMENT_FORBIDDEN)
+        if (existsReportByUserIdAndCommentId(userId, reportedComment.id!!))
+            throw BadRequestException(ExceptionType.REPORT_COMMENT_BAD_REQUEST)
         reportRepository.save(Report(userId = userId, reportedCommentId = commentId))
+        if (reportedComment.isBlinded) return
         val cnt: Long = reportRepository.countByReportedCommentId(commentId)
-        if(cnt >= BLINDED_CONDITION) reportedComment.changeBlindState(BLINDED_STATE)
+        if (cnt >= BLINDED_CONDITION) reportedComment.changeBlindState(BLINDED_STATE)
+    }
+
+    fun existsReportByUserIdAndCommentId(userId: Long, commentId: Long): Boolean {
+        if (reportRepository.existsByUserIdAndReportedCommentId(userId, commentId)) return true
+        return false
     }
 }
