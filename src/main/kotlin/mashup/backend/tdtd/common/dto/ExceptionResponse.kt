811acfb03677d79f59e6a78676f8e2b49d955215
YJeongKim <yjeongisme@gmail.com>
2021-04-01 22:14:22 +0900

    fix: 사용하지 않는 ExceptionResponse 삭제

--- src/main/kotlin/mashup/backend/tdtd/common/dto/ExceptionResponse.kt
+++
@@ -1,9 +0,0 @@
-package mashup.backend.tdtd.common.dto
-
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
-import com.fasterxml.jackson.databind.annotation.JsonNaming
-
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
-class ExceptionResponse(
-    val errMsg: String
-)
