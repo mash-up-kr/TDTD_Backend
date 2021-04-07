dcdcbc4379eb9f27a35b80decc4a755e09e314e0
jong yoon <dhorlawhddbs@naver.com>
2021-04-07 16:10:51 +0900

    feat: 동적 링크에 안드로이드 앱 네임 추가

--- src/main/kotlin/mashup/backend/tdtd/common/util/DynamicLinkGenerator.kt
+++ src/main/kotlin/mashup/backend/tdtd/common/util/DynamicLinkGenerator.kt
@@ -9,10 +9,12 @@ class DynamicLinkGenerator {
         private const val LINK = "http://52.79.54.20/dynamic"
         private const val IOS_APP_STORE_ID = "1557251130"
         private const val IOS_APP_BUNDLE_ID = "com.chan.TDTD"
+        private const val AND_APP_PACKAGE_NAME = "com.tdtd.voicepaper"
 
         fun getLongLink(roomCode: String): String {
             return UriComponentsBuilder.fromUriString("$SCHEMA://$HOST/")
                 .queryParam("link", LINK)
+                .queryParam("apn", AND_APP_PACKAGE_NAME)
                 .queryParam("isi", IOS_APP_STORE_ID)
                 .queryParam("ibi", IOS_APP_BUNDLE_ID)
                 .queryParam("room-code", roomCode)
