f2bc18d5a2681e35a02a2dccf6086ac6f3f29900
jong yoon <dhorlawhddbs@naver.com>
2021-04-23 16:36:16 +0900

    fix : seed data 수정
    - 로컬에서만 실행하도록 변경

--- src/main/kotlin/mashup/backend/tdtd/common/util/SeedDataInitializer.kt
+++ src/main/kotlin/mashup/backend/tdtd/common/util/SeedDataInitializer.kt
@@ -14,6 +14,7 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
+@Profile("local")
 @Component
 class SeedDataInitializer(
     private val userService: UserService,
