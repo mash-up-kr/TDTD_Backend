536d45bc7efd28dc36ed84d91652b683dc6fc2d9
YJeongKim <yjeongisme@gmail.com>
2021-10-07 03:32:48 +0900

    feat: JPA Auditing 활성화

---
+++ src/main/kotlin/mashup/backend/tdtd/config/JpaConfig.kt
@@ -0,0 +1,9 @@
+package mashup.backend.tdtd.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig {
+}
